8c3dc45c0a81bee08cca61bd496ed7df8bc6e62b
SvtFisher <b92mvf@narod.ru>
2023-06-19 23:20:48 +0700

    Д/З урок Методы с правками. Финальная версия.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         printNumberTask(1) ;
         checkingLongYear(2023);
         printNumberTask(2);
-        anableCklientPhone(2016,0);
+        anableCklientPhone(2014,2);
         printNumberTask(3);
         anableDistanceToCklient(95);
     }
@@ -16,23 +16,23 @@ public class Main {
     }
     public static void anableCklientPhone(int clientDeviceYear, int clientOS) {
         int Year = 2015 ;
-        int OS = 0 ;
-        int OS2 = 1 ;
-        if (clientOS != OS && clientOS != OS2 ) {
+        int iOs = 0 ;
+        int android = 1 ;
+        if (clientOS != iOs && clientOS != android ) {
             System.out.println("Операционная система не поддерживается.");
         } else if ( clientDeviceYear < Year ) {
-            if (clientOS == OS) {
-                System.out.println("Установите облегчённую версию приложения для iOS по ссылке.");
+            if (clientOS == iOs) {
+                System.out.println("Установите облегчённую версию приложения для iOs по ссылке.");
             } else {
                 System.out.println("Установите облегчённую версию приложения для Android по ссылке.");
             }
-        } else if (clientOS == OS2) {
+        } else if (clientOS == android) {
             System.out.println("Установите версию приложения для Android.");
         } else {
-            System.out.println("Установите версию приложения для iOS по ссылке.");
+            System.out.println("Установите версию приложения для iOs по ссылке.");
         }
     }
-    public static int anableDistanceToCklient(int deliveryDistance) {
+    public static int anableDistanceToCklient (int deliveryDistance) {
         int deliveryDays = 0 ;
         if ( deliveryDistance > 100 ) {
             deliveryDays = -1 ;
@@ -43,12 +43,12 @@ public class Main {
         } else if( deliveryDistance > 60 && deliveryDistance < 100 ) {
             deliveryDays = 3 ;
         }
-        System.out.println("Доставка займёт: " + deliveryDays + " дня(дней)."  );
+        System.out.println("Доставка займёт: " + deliveryDays + " дня(день,дней)."  );
         return deliveryDays ;
     }
     public static void printNumberTask(int num) {
         System.out.println("Задача № " + num + ".") ;
     }
 }
-// Домашнее задание выполненно с учётом замечаний.
+// Домашнее задание выполненно с учётом замечаний, финальня версия.
 // Домашнее задание считаю выполненным.
